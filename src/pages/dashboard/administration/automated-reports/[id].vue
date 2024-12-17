a9a74099a144caefe1c69a7c2763fa0ff826b136
AJ <ajohnson@sourceflow.com>
2024-12-17 10:11:08 -0700

    Added comments about the TS errors

--- src/pages/dashboard/administration/automated-reports/[id].vue
+++ src/pages/dashboard/administration/automated-reports/[id].vue
@@ -2,11 +2,15 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 
+// this produces a typescript error
+// Argument of type '"/dashboard/administration/automated-reports/:id"' is not assignable to parameter of type 'keyof RouteNamedMap | undefined'.ts(2345)
 const route = useRoute('/dashboard/administration/automated-reports/:id')
 </script>
 
 <template>
   <div>
+    <!-- Property 'id' does not exist on type 'Record<never, never> | { id: string; } | { id: string; }'.
+  Property 'id' does not exist on type 'Record<never, never>'.ts(2339) -->
     <h1>Automated Report {{ route.params.id }}</h1>
     <router-link to="/">Home</router-link>
   </div>
--- src/pages/dashboard/administration/users/[id]/index.vue
+++ src/pages/dashboard/administration/users/[id]/index.vue
@@ -2,11 +2,15 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 
+// this produces a typescript error
+// Argument of type '"/dashboard/administration/users/:id"' is not assignable to parameter of type 'keyof RouteNamedMap | undefined'.ts(2345)
 const route = useRoute('/dashboard/administration/users/:id')
 </script>
 
 <template>
   <div>
+        <!-- Property 'id' does not exist on type 'Record<never, never> | { id: string; } | { id: string; }'.
+  Property 'id' does not exist on type 'Record<never, never>'.ts(2339) -->
     <h1>User ID: {{ route.params.id }}</h1>
     <router-link to="/">Home</router-link>
   </div>
